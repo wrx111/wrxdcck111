db5b048cf1602d99945d7f05d07e56fcb727b0bb
WRX <2537715648@qq.com>
2020-11-13 08:45:34 +0800

    This is the first commit.

---
+++ app/src/main/java/com/example/helloworld/FirstActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout1);
+    }
+
+}
